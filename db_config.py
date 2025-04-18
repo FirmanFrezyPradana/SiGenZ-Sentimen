09d7de98e8b68fae7b196e90de7a0d8063b6f97b
Firman Frezy Pradana <110784278+FirmanFrezyPradana@users.noreply.github.com>
2025-04-18 17:33:39 +0700

    handle data besar

--- db_config.py
+++ db_config.py
@@ -12,6 +12,11 @@ def connect_db():
         connection = mysql.connector.connect(**DB_CONFIG)
         if connection.is_connected():
             print("✅ Berhasil terhubung ke database MySQL!")
+
+             # Set nilai max_allowed_packet jadi 64MB (67108864 byte)
+            cursor = connection.cursor()
+            cursor.execute("SET GLOBAL max_allowed_packet=67108864")
+            cursor.close()
         return connection
     except mysql.connector.Error as err:
         print(f"❌ Error connecting to MySQL: {err}")
