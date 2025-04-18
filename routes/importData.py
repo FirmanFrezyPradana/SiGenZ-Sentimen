0bc52386caead633502e8eb5987bd1a3ba70d5a4
Firman Frezy Pradana <110784278+FirmanFrezyPradana@users.noreply.github.com>
2025-04-19 00:40:06 +0700

    fix&style(import data & preprocessing) : merapikan source code dan menghapus stopword secara manual

--- routes/importData.py
+++ routes/importData.py
@@ -1,12 +1,16 @@
 import csv
 from io import TextIOWrapper
-from flask import Blueprint, request, jsonify,redirect, url_for,render_template, flash
+from flask import Blueprint, request,redirect, url_for, flash
 from db_config import connect_db
 
 import_bp = Blueprint('importData', __name__)
 
 @import_bp.route('/import', methods=['POST'])
 def import_csv():
+    connection = connect_db()
+    if not connection:
+        flash('Koneksi ke database gagal', 'error')
+        return redirect(url_for('dataSentimen.dataSentimen'))
     if 'file' not in request.files:
          flash('Tidak ada file yang dipilih atau file kosong', 'error')
          return redirect(url_for('dataSentimen.dataSentimen'))
@@ -16,23 +20,14 @@ def import_csv():
     if not file.filename.lower().endswith('.csv'):
          flash('File yang diunggah harus berformat CSV', 'error')
          return redirect(url_for('dataSentimen.dataSentimen'))
-
-    connection = connect_db()
-    if not connection:
-        flash('Koneksi ke database gagal', 'error')
-        return redirect(url_for('dataSentimen.dataSentimen'))
-
     try:
         file_stream = TextIOWrapper(file.stream, encoding='utf-8')
         reader = csv.DictReader(file_stream)
-
         required_columns = ['teks', 'sosmed', 'labels']
         if set(reader.fieldnames) - {'no'}!= set(required_columns):
             flash('Format CSV tidak valid. Hanya kolom "teks", "sosmed", dan "labels" yang diperbolehkan', 'error')
             return redirect(url_for('dataSentimen.dataSentimen'))
-
         cursor = connection.cursor()
-
         for row in reader:
             teks = (row['teks'] or "").strip().strip('"')
 
@@ -41,16 +36,12 @@ def import_csv():
                 query = "INSERT INTO data_sentimen (teks, sosmed, labels) VALUES (%s, %s, %s)"
                 values = (teks, row['sosmed'], row['labels'])
                 cursor.execute(query, values)
-
         connection.commit()
         cursor.close()
         connection.close()
-
          # Flash success message
         flash('File berhasil diunggah dan data berhasil dimasukkan.', 'success')
-
         return redirect(url_for('dataSentimen.dataSentimen'))   # Kembali ke halaman import.html
-
     except Exception as e:
         flash(f'Terjadi kesalahan: {str(e)}', 'error')
         return redirect(url_for('dataSentimen.dataSentimen'))
--- routes/preprocessing.py
+++ routes/preprocessing.py
@@ -27,6 +27,7 @@ def page_preprocessing():
 # Cleaning text
 def cleaning_text(text):
     text = re.sub(r'http\S+|www\.\S+', '', text)
+    text = re.sub(r'<br>', ' ', text)
     text = re.sub(r'<.*?>', '', text)
     text = re.sub(r'@\S+', '', text)
     text = re.sub(r'['
@@ -35,7 +36,6 @@ def cleaning_text(text):
                    u'\U0001F680-\U0001F6FF'
                    u'\U0001F1E0-\U0001F1FF'
                     ']+', '', text)
-
     text = re.sub(r'\d+', '', text)
     text = re.sub(r'\b(\w+)-\1\b', r'\1', text)
     text = re.sub(r'[^a-zA-Z\s]', ' ', text)
@@ -55,84 +55,20 @@ def normalisasi(text):
         return [normalisasi_kata_dict.get(word, word) for word in text]
     return text
 
-# # Stopword removal library
+# # Stopword
 def stopword_removal(text):
     try:
         factory = StopWordRemoverFactory()
         stopword = factory.get_stop_words()
         more_stopword = [
-            "ada", "adalah", "adanya", "adapun", "agak", "agaknya", "agar", "akan", "akankah",
-            "akhir", "akhiri", "akhirnya", "aku", "akulah", "amat", "amatlah", "anda", "andalah",
-            "antar", "antara", "antaranya", "apa", "apaan", "apabila", "apakah", "apalagi", "apatah",
-            "artinya", "asal", "asalkan", "atas", "atau", "ataukah", "ataupun", "awal", "awalnya",
-            "bagai", "bagaikan", "bagaimana", "bagaimanakah", "bagaimanapun", "bagi", "bagian", "bahkan",
-            "bahwa", "bahwasanya", "baik", "bakal", "bakalan", "balik", "banyak", "bapak", "baru", "bawah",
-            "beberapa", "begini", "beginian", "beginikah", "beginilah", "begitu", "begitukah", "begitulah",
-            "begitupun", "bekerja", "belakang", "belakangan", "belum", "belumlah", "benar", "benarkah",
-            "benarlah", "berada", "berakhir", "berakhirlah", "berakhirnya", "berapa", "berapakah", "berapalah",
-            "berapapun", "berarti", "berawal", "berbagai", "berdatangan", "beri", "berikan", "berikut",
-            "berikutnya", "berjumlah", "berkali-kali", "berkata", "berkehendak", "berkeinginan", "berkenaan",
-            "berlainan", "berlalu", "berlangsung", "berlebihan", "bermacam", "bermacam-macam", "bermaksud",
-            "bermula", "bersama", "bersama-sama", "bersiap", "bersiap-siap", "bertanya", "bertanya-tanya",
-            "berturut", "berturut-turut", "bertutur", "berujar", "berupa", "besar", "betul", "betulkah",
-            "biasa", "biasanya", "bila", "bilakah", "bisa", "bisakah", "boleh", "bolehkah", "bolehlah", "buat",
-            "bukan", "bukankah", "bukanlah", "bukannya", "bulan", "bung", "cara", "caranya", "cukup", "cukupkah",
-            "cukuplah", "cuma", "dahulu", "dalam", "dan", "dapat", "dari", "daripada", "datang", "dekat", "demi",
-            "demikian", "demikianlah", "dengan", "depan", "di", "dia", "diakhiri", "diakhirinya", "dialah",
-            "diantara", "diantaranya", "diberi", "diberikan", "diberikannya", "dibuat", "dibuatnya", "didapat",
-            "didatangkan", "digunakan", "diibaratkan", "diibaratkannya", "diingat", "diingatkan", "diinginkan",
-            "dijawab", "dijelaskan", "dijelaskannya", "dikarenakan", "dikatakan", "dikatakannya", "dikerjakan",
-            "diketahui", "diketahuinya", "dikira", "dilakukan", "dilalui", "dilihat", "dimaksud", "dimaksudkan",
-            "dimaksudkannya", "dimaksudnya", "diminta", "dimintai", "dimisalkan", "dimulai", "dimulailah",
-            "dimulainya", "dimungkinkan", "dini", "dipastikan", "diperbuat", "diperbuatnya", "dipergunakan",
-            "diperkirakan", "diperlihatkan", "diperlukan", "diperlukannya", "dipersoalkan", "dipertanyakan",
-            "dipunyai", "diri", "dirinya", "disampaikan", "disebut", "disebutkan", "disebutkannya", "disini",
-            "disinilah", "ditambahkan", "ditandaskan", "ditanya", "ditanyai", "ditanyakan", "ditegaskan",
-            "ditujukan", "ditunjuk", "ditunjuki", "ditunjukkan", "ditunjukkannya", "ditunjuknya", "dituturkan",
-            "dituturkannya", "diucapkan", "diucapkannya", "diungkapkan", "dong", "dua", "dulu", "empat", "enggak",
-            "enggaknya", "entah", "entahlah", "guna", "gunakan", "hal", "hampir", "hanya", "hanyalah", "hari",
-            "harus", "haruslah", "harusnya", "hendak", "hendaklah", "hendaknya", "hingga", "ia", "ialah", "ibarat",
-            "ibaratkan", "ibaratnya", "ibu", "ikut", "ingat", "ingat-ingat", "ingin", "inginkah", "inginkan",
-            "ini", "inikah", "inilah", "itu", "itukah", "itulah", "jadi", "jadilah", "jadinya", "jangan", "jangankan",
-            "janganlah", "jauh", "jawab", "jawaban", "jawabnya", "jelas", "jelaskan", "jelaslah", "jelasnya", "jika",
-            "jikalau", "juga", "jumlah", "jumlahnya", "justru", "kala", "kalau", "kalaulah", "kalaupun", "kalian",
-            "kami", "kamilah", "kamu", "kamulah", "kan", "kapan", "kapankah", "kapanpun", "karena", "karenanya",
-            "kasus", "kata", "katakan", "katakanlah", "katanya", "ke", "keadaan", "kebetulan", "kecil", "kedua",
-            "keduanya", "keinginan", "kelamaan", "kelihatan", "kelihatannya", "kelima", "keluar", "kembali",
-            "kemudian", "kemungkinan", "kemungkinannya", "kenapa", "kepada", "kepadanya", "kesampaian",
-            "keseluruhan", "keseluruhannya", "keterlaluan", "ketika", "khususnya", "kini", "kinilah", "kira",
-            "kira-kira", "kiranya", "kita", "kitalah", "kok", "kurang", "lagi", "lagian", "lah", "lain", "lainnya",
-            "lalu", "lama", "lamanya", "lanjut", "lanjutnya", "lebih", "lewat", "lima", "luar", "macam", "maka",
-            "makanya", "makin", "malah", "malahan", "mampu", "mampukah", "mana", "manakala", "manalagi", "masa",
-            "masalah", "masalahnya", "masih", "masihkah", "masing", "masing-masing", "mau", "maupun", "melainkan",
-            "melakukan", "melalui", "melihat", "melihatnya", "memang", "memastikan", "memberi", "memberikan",
-            "membuat", "memerlukan", "memihak", "meminta", "memintakan", "memisalkan", "memperbuat", "mempergunakan",
-            "memperkirakan", "memperlihatkan", "mempersiapkan", "mempersoalkan", "mempertanyakan", "mempunyai",
-            "memulai", "memungkinkan", "menaiki", "menambahkan", "menandaskan", "menanti", "menanti-nanti",
-            "menantikan", "menanya", "menanyai", "menanyakan", "mendapat", "mendapatkan", "mendatang", "mendatangi",
-            "mendatangkan", "menegaskan", "mengakhiri", "mengapa", "mengatakan", "mengatakannya", "mengenai",
-            "mengerjakan", "mengetahui", "menggunakan", "menghendaki", "mengibaratkan", "mengibaratkannya",
-            "mengingat", "mengingatkan", "menginginkan", "mengira", "mengucapkan", "mengucapkannya", "mengungkapkan",
-            "menjadi", "menjawab", "menjelaskan", "menuju", "menunjuk", "menunjuki", "menunjukkan", "menuturkan",
-            "menyampaikan", "menyatakan", "menyebut", "menyebutkan", "menyebutkannya", "menyembunyikan",
-            "menyertakan", "merasa", "merupakan", "meskipun", "mereka", "merekalah", "merupakan", "meskipun",
-            "mesti", "mungkin", "mungkinkah", "mungkinlah", "nah", "nama", "nanti", "nanti-nanti", "nantinya",
-            "negeri", "nggak", "nggaknya", "ni", "no", "oleh", "olehkarena", "olehkarenaitu", "olehsebabitu",
-            "pada", "padahal", "pada", "paling", "paling-paling", "palingnya", "para", "penting", "per", "pergi",
-            "pertama", "pertama-tama", "perusahaan", "pihak", "pihaknya", "proses", "rasa", "rasanya", "saat", "saatnya",
-            "saja", "sajalah", "salam", "sama", "samalah", "sangat", "sangatlah", "sangatpun", "saya", "sayalah", "se",
-            "seakan", "seakan-akan", "seandainya", "sebaliknya", "sebagaimana", "sebagaimanapun", "sebagian",
-            "sebaliknya", "sebelum", "sebelumnya", "sebentar", "sebuah", "sebuahnya", "sehingga", "sekadar",
-            "sekali", "sekalipun", "sekarang", "sekaranglah", "sekarangpun", "seperti", "sepertinya", "sepuluh",
-            "sesama", "sesuatu", "sesuatunya", "seusia", "setelah", "setelahnya", "setiap", "setidaknya",
-            "setuju", "setujuan", "siapa", "siapakah", "siapapun", "sini", "sinilah", "siswa", "sudah", "sudahlah",
-            "sudahkan", "sudahkah", "sudahpun", "suka", "sukakah", "sukanya", "supaya", "tak", "tapi", "tahap", "tahapnya",
-            "tanpa", "tanya", "tanyakan", "tanyanya", "tempat", "terakhir", "terakhirnya", "terdapat", "terdapatkan",
-            "terhadap", "terhadapnya", "terjadi", "terjadilah", "terjadinya", "terlihat", "terlihatnya", "termasuk",
-            "ternyata", "tersebut", "tersebutlah", "tersedia", "terserah", "terus", "teruslah", "terusnya",
-            "tetap", "tetaplah", "tetapnya", "tidak", "tidakkah", "tidaklah", "tidaknya", "tinggal", "tinggalah",
-            "tujuh", "tunggu", "tunggu-tunggu", "tungguin", "tungguinlah", "untuk", "untuknya", "usai", "usah", "yaitu",
-            "yakin", "yakinkah", "yakinkan", "mu","nya","an","bang",
+            "a", "ak", "abc", "acc", "acoe", "ah", "bola", "bla", "uec", "beuh", "ayo", "bu", "b", "acom", "hahahahha","dekkkk","pol","gemezzz",
+            "ada", "adu", "oe", "aldo", "akh", "aka", "aki", "all", "dmi", "enji", "khao", "urgh", "haha", "wkwkwkwkwkwk", "hhhhhhhhh","dekkkkkkkk",
+            "cam", "ae", "alert", "afer", "agreed", "aides", "akn", "alur", "do", "eq", "fittest", "ishhh", "eh", "het", "wah","x","kan","ku","nih",
+            "cahol", "aegyo", "aer", "ajer", "ai", "aids", "ala", "am", "dok", "etc", "anaoisanya", "wkwk", "aowkaowk", "uh", "ai","main fan fiction",
+            "c", "camp", "can", "carats", "cc", "cek", "cekadak", "dik", "dong", "hoho", "euy", "ohhh", "amp", "ohhhhh", "amp","an","aaja","brooooo gas oke gas in",
+            "centris", "centu", "ceo", "ckckck", "cot", "core", "deri", "dl", "duh", "belva", "wooooww", "ozaaaa", "wah", "isee",
+            "cuba", "cuihh", "da", "dah", "bim", "deh", "diding", "dlk", "e", "quot", "ber", "uuhhh", "laaaaaaaaaaaaaaaaaaah",
+            "cucuk", "cuy", "daei", "dahh", "dangg", "dek", "dih", "dm", "eh", "beibeh", "huuaaaaa", "hmmm", "wkwkwkwkwkwkw","amp","sih","nya","aku", "tahun","jam","pagi","coba","waktu","namanya","mulai","sore","anak","orang","kayak","intinya","semua"
         ]
         stop_words = factory.get_stop_words() + more_stopword
         return [word for word in text if word not in stop_words]
@@ -159,24 +95,21 @@ def preprocess_texts(teks, labels):
         df['normalized_text'] = df['tokenized_text'].apply(normalisasi)
         df['stopword_text'] = df['normalized_text'].apply(stopword_removal)
         df['lemmatized_text'] = df['stopword_text'].apply(stemmed)
-
         return df
     except Exception as e:
-        flash(f"Error in preprocessing: {e}", "danger")
+        flash(f"terjadi kesalahan saat preprocessing data: {e}", "danger")
         return None
 
 @preprocessing_bp.route('/preprocessing/proses', methods=['GET', 'POST'])
 def preprocessing_data():
     if request.method == 'GET':
         return render_template("pre-processing.html")
-
     try:
         # Ambil data dari database
         connection = connect_db()
         with connection.cursor() as cursor:
             cursor.execute("SELECT teks,labels FROM data_sentimen")
             data_sentimen = [{"teks": row[0], "labels": row[1]} for row in cursor.fetchall()]
-        connection.close()
 
         # Cek apakah data kosong
         if not data_sentimen:
@@ -185,24 +118,13 @@ def preprocessing_data():
 
         # Ubah hasil query menjadi DataFrame
         df = pd.DataFrame(data_sentimen, columns=["teks","labels"])
-        # df["teks"] = df["teks"].astype(str)  # Pastikan bertipe string
-        # df = preprocess_texts(df["teks"].tolist())  # Preprocessing teks
-
         teks_list = [item['teks'] for item in data_sentimen]
         labels_list = [item['labels'] for item in data_sentimen]
         df = preprocess_texts(teks_list, labels_list)
 
-
-
-
-        # Simpan hasil ke database, cek duplikasi terlebih dahulu
-        connection = connect_db()
+        # Simpan hasil ke database
         with connection.cursor() as cursor:
-            # Hapus teks yang sudah ada di preprocessing agar tidak duplikat
-            cursor.execute("DELETE FROM preprocessing WHERE teks IN (SELECT teks FROM data_sentimen)")
-            connection.commit()
-
-            # Masukkan data yang belum ada
+            cursor.execute("TRUNCATE TABLE preprocessing")
             cursor.executemany(
                 """
                 INSERT INTO preprocessing (teks,labels, cleaned_text, lower_text, tokenized_text, normalized_text, stopword_text, preprocessing_text)
@@ -213,20 +135,21 @@ def preprocessing_data():
                   ' '.join(row["stopword_text"]), ' '.join(row["lemmatized_text"]))
                  for _, row in df.iterrows()]
             )
-            connection.commit()
 
-            # Hapus duplikat dalam tabel preprocessing, simpan satu saja
+            # Hapus duplikat dalam tabel preprocessing
             cursor.execute("""
                 DELETE FROM preprocessing
                 WHERE id NOT IN (
                     SELECT MIN(id) FROM preprocessing GROUP BY teks
                 )
             """)
+            # hapus data yang ada pada tabel
+            cursor.execute("TRUNCATE TABLE data_tfidf")
             connection.commit()
 
         connection.close()
         flash("Preprocessing berhasil dilakukan!", "success")
-        return redirect(url_for('preprocessing.page_preprocessing'))
+        return redirect(url_for('preprocessing.page_preprocessing', status='preprocess_success'))
 
     except Exception as error:
         flash(f"Terjadi kesalahan: {error}", "danger")
