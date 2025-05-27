e0d97b6ea829facafb325699c1428f4678f96a33
Firman Frezy Pradana <110784278+FirmanFrezyPradana@users.noreply.github.com>
2025-05-27 20:39:35 +0700

    delete(importData):menghapus importData karena sudah menjadi satu pada file data_sentimen

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,5 @@
 from .dashboard import dashboard_bp
 from .dataSentimen import dataSentimen_bp
-from .importData import import_bp
 from .preprocessing import preprocessing_bp
 from .pembobotan import pembobotan_bp
 from .dataTraining import dataTraining_bp
@@ -8,24 +7,12 @@ from .dataTesting import dataTesting_bp
 from .implementasi import implementasiSvm_bp
 from .cek import cek_bp
 
-# from .traintest import traintest_bp
-# from .testresult import testresult_bp
-# from .register import register_bp
-# from .login import login_bp
-# from .admin_routes import admin_bp
-
 def register_blueprints(app):
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(dataSentimen_bp)
-    app.register_blueprint(import_bp)
     app.register_blueprint(preprocessing_bp)
     app.register_blueprint(pembobotan_bp)
     app.register_blueprint(dataTraining_bp)
     app.register_blueprint(dataTesting_bp)
     app.register_blueprint(implementasiSvm_bp)
     app.register_blueprint(cek_bp)
-
-    # app.register_blueprint(testresult_bp)
-    # app.register_blueprint(register_bp)
-    # app.register_blueprint(login_bp)
-    # app.register_blueprint(admin_bp)
