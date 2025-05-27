db070d50b18937d8202d692e5d20ea964f4d2246
Firman Frezy Pradana <110784278+FirmanFrezyPradana@users.noreply.github.com>
2025-05-27 20:30:15 +0700

    style(data_sentimen):menambhakan loading

--- routes/dataSentimen.py
+++ routes/dataSentimen.py
@@ -15,49 +15,6 @@ def dataSentimen():
 
 @dataSentimen_bp.route('/import_data', methods=['POST'])
 def import_data():
-    # if 'file' not in request.files:
-    #     flash('File tidak ditemukan', 'error')
-    #     return redirect(url_for('dataSentimen.dataSentimen'))
-
-    # file = request.files['file']
-
-    # if not file.filename.lower().endswith('.csv'):
-    #     flash('File harus berformat CSV', 'error')
-    #     return redirect(url_for('dataSentimen.dataSentimen'))
-
-    # try:
-    #     file_stream = TextIOWrapper(file.stream, encoding='utf-8')
-    #     reader = csv.DictReader(file_stream)
-    #     required_columns = ['teks', 'sosmed', 'labels']
-
-    #     # Validasi kolom
-    #     if not set(required_columns).issubset(reader.fieldnames):
-    #         flash('Kolom "teks", "sosmed", dan "labels" harus ada.', 'error')
-    #         return redirect(url_for('dataSentimen.dataSentimen'))
-
-    #     # Ambil semua teks yang sudah ada di DB untuk menghindari duplikasi
-    #     existing_teks = set(row[0] for row in db.session.query(DataSentimen.teks).all())
-
-    #     new_data = []
-    #     for row in reader:
-    #         teks = (row['teks'] or '').strip().strip('"')
-    #         sosmed = (row['sosmed'] or '').strip()
-    #         labels = (row['labels'] or '').strip()
-
-    #         if teks and teks not in existing_teks:
-    #             new_data.append(DataSentimen(teks=teks, sosmed=sosmed, labels=labels))
-
-    #     if new_data:
-    #         db.session.bulk_save_objects(new_data)
-    #         db.session.commit()
-    #         flash(f'Berhasil menambahkan {len(new_data)} data baru.', 'success')
-    #     else:
-    #         flash('Semua data sudah ada atau kosong.', 'warning')
-
-    # except Exception as e:
-    #     db.session.rollback()
-    #     flash(f'Terjadi kesalahan saat mengimpor: {str(e)}', 'error')
-
     if request.method == "POST" and "file" in request.files:
             file = request.files["file"]
 
@@ -88,27 +45,25 @@ def import_data():
                         db.session.commit()
                         flash(f'Berhasil menambahkan {len(new_data)} data baru.', 'success')
                     else:
-                        flash('Semua data sudah ada atau kosong.', 'warning')
+                        flash('Semua data sudah ada atau kosong.', 'error')
 
                 except Exception as e:
                     db.session.rollback()
                     flash(f'Gagal mengimpor data: {str(e)}', 'danger')
-
             else:
-                flash('File harus berupa CSV.', 'warning')
-
+                flash('File harus berupa CSV.', 'error')
     return redirect(url_for('dataSentimen.dataSentimen', status='import_success'))
 
 
 @dataSentimen_bp.route('/dataSentimen/Delete_all', methods=['POST'])
 def delete_all_data():
     try:
-        delete = db.session.query(DataSentimen).delete()
+        db.session.query(DataSentimen).delete()
         db.session.commit()
-        flash(f'{delete} data berhasil dihapus!', 'success')
+        flash('data berhasil dihapus!', 'success')
     except Exception as e:
         db.session.rollback()
-        flash(f'Terjadi kesalahan: {str(e)}', 'error')
+        flash({'status': 'error', 'message': f'Terjadi kesalahan: {str(e)}'})
 
     return redirect(url_for('dataSentimen.dataSentimen'))
 
