d803d153d92ffcd83aa6c7ed65a3adc972502e56
Firman Frezy Pradana <110784278+FirmanFrezyPradana@users.noreply.github.com>
2025-07-24 23:12:49 +0700

    tambah fitur cek kalimat

--- routes/cek.py
+++ routes/cek.py
@@ -1,41 +1,41 @@
 # from tfidf import transform
 from flask import Blueprint
-from flask import jsonify
+from flask import jsonify ,render_template,request
 import pickle
 from preprocessing import preprocess_texts
 
 cek_bp = Blueprint('cek', __name__)
 
-@cek_bp.route('/cek', methods=['GET'])
+@cek_bp.route('/cek', methods=['POST','GET'])
 def cek():
-    # kalimat uji dan preprocessing
-    kalimat_baru = ["gen z tolol banget apa pa harus atasan emang gen z gabisa kerja"]
-    df = preprocess_texts(kalimat_baru)
+    label_prediksi = ''
+    preprocessing_teks = ''
+    teks = ''
+    teks = request.form.get('teks','')
+    if teks:
+        df = preprocess_texts([teks])
+        # karena hanya mengambil 1 baris makan gunakan .iloc[0]
+        preprocessing_teks = df['preprocessing_text'].iloc[0]
 
-    # karena hanya mengambil 1 baris makan gunakan .iloc[0]
-    teks_baru = df['preprocessing_text'].iloc[0]
+        # menangani teks yang berupa list
+        if isinstance(preprocessing_teks, list):
+            preprocessing_teks = ' '.join(preprocessing_teks)
 
-    # menangani teks yang berupa list
-    if isinstance(teks_baru, list):
-        teks_baru = ' '.join(teks_baru)
+        # Load TF-IDF vectorizer
+        with open('static/model/tfidf_vectorizer.pkl', 'rb') as vec_file:
+            vectorizer = pickle.load(vec_file)
 
-     # Load TF-IDF vectorizer
-    with open('static/model/tfidf_vectorizer.pkl', 'rb') as vec_file:
-        vectorizer = pickle.load(vec_file)
+        # Transformasi teks uji ke bentuk TF-IDF
+        hasil_tfidf = vectorizer.transform([preprocessing_teks])
 
-    # Transformasi teks uji ke bentuk TF-IDF
-    hasil_tfidf = vectorizer.transform([teks_baru])
+        # Load model SVM
+        with open('static/model/model_svm_linear.pkl', 'rb') as model_file:
+            loaded_linear_model = pickle.load(model_file)
 
-    # Load model SVM
-    with open('static/model/model_svm_linear.pkl', 'rb') as model_file:
-        loaded_linear_model = pickle.load(model_file)
+        # prediksi
+        hasil_linear = loaded_linear_model.predict(hasil_tfidf)
+        label_prediksi = "Positif" if hasil_linear[0] == 1 else "Negatif"
 
-    # prediksi
-    hasil_linear = loaded_linear_model.predict(hasil_tfidf)
-    label_prediksi = "Positif" if hasil_linear[0] == 1 else "Negatif"
 
-    return jsonify({
-        'teks_preprocessing': teks_baru,
-        'prediksi': label_prediksi,
-    })
+    return render_template('cekKalimat.html',teks=teks, preprocessing_teks=preprocessing_teks,label_prediksi=label_prediksi)
 
